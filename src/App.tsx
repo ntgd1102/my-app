2167668c35d4bfdb400c32278b929933688e74a3
ntgd1102 <ntgd1102@gmail.com>
2022-07-13 22:00:48 -0700

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const wantToFly = 'fly2222';
+  const wantToFly = 'ly22';
   return (
     <div className="App">
       <header className="App-header">
